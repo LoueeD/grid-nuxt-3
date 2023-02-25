1dcefa0fd44c5df699117baf14007c5a953c86de
LoueeD <loueed@gmail.com>
2023-02-25 23:11:04 +0000

    Initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="site">
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+
+  .site {
+    min-height: 100vh;
+  }
+}
+</style>
